f728f42e2d08a7e51e4a47236d67fb13e6a4f8ab
Heidi Schultz <heidischultz23@gmail.com>
2023-09-01 10:26:24 -0400

    sign up functionality and positioning onboarding 2

--- Erra/Views/Onboarding2.swift
+++ Erra/Views/Onboarding2.swift
@@ -9,110 +9,123 @@ import SwiftUI
 
 struct Onboarding2: View {
     @StateObject private var viewModel1 = Login1ViewModel()
-
+    @State private var showOnboarding3 = false
+    
     var body: some View {
-        GeometryReader { geometry in
-            
-            ZStack{
-                VStack{
-                    Image("onboardFire")
-                        .resizable()
-                        .aspectRatio(contentMode:.fill)
-                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.55)
+        NavigationView{
+            GeometryReader { geometry in
+                
+                ZStack{
+                    VStack{
+                        Image("onboardFire")
+                            .resizable()
+                            .aspectRatio(contentMode:.fill)
+                            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.55)
+                        
+                        Rectangle()
+                            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.45)
+                            .foregroundColor(.white)
+                        
+                        
+                        
+                    }
                     
                     Rectangle()
-                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.45)
+                        .frame(width: UIScreen.main.bounds.width * 0.90, height: UIScreen.main.bounds.height * 0.30)
                         .foregroundColor(.white)
+                        .cornerRadius(8)
                     
-                    
-                    
-                }
-                
-                Rectangle()
-                    .frame(width: UIScreen.main.bounds.width * 0.90, height: UIScreen.main.bounds.height * 0.15)
-                    .foregroundColor(.white)
-                    .cornerRadius(8)
-               
                     Image("Logo")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(width:200, height:200)
-                        .position(x:290 ,y: 440)
-           
-                
-                    Text("Let Ignis get to know you!")
-                        .font(Font.custom("Inter-Regular",size: 24))
-                        .frame(width: UIScreen.main.bounds.width * 0.93)
-                        .padding(.top, 60)
+                        .frame(width:210, height:200)
+                        .offset(x:70 ,y: -50)
+//                        .position(x:290 ,y: 440)
+                    
                     
             
-                //LOGIN LOGIN LOGIN //
-                //pls help me position this right
-            
-                VStack {
-                  
-                    TextField("Enter Your Email", text: $viewModel1.email)
-                        .padding()
-                        .background(
-                            VStack {
-                                Color.white.opacity(0.4)
-                                Rectangle().frame(height: 1).foregroundColor(.gray)
-                            }
-                        )
                     
-                    TextField("Enter Your Name", text: $viewModel1.name)
-                        .padding()
-                        .background(
-                            VStack {
-                                Color.white.opacity(0.4)
-                                Rectangle().frame(height: 1).foregroundColor(.gray)
-                            }
-                        )
-                                            
-                    SecureField("Enter Your Password", text: $viewModel1.password)
-                        .padding()
-                        .background(
-                            VStack {
-                                Color.white.opacity(0.4)
-                                Rectangle().frame(height: 1).foregroundColor(.gray)
+                    
+                    //LOGIN LOGIN LOGIN //
+                    //pls help me position this right
+                    
+                    VStack {
+                        Text("Let Ignis get to know you!")
+                            .font(Font.custom("Inter-Regular",size: 24))
+                            .frame(width: UIScreen.main.bounds.width * 0.93)
+                            .padding(.top, 60)
+                        
+                        TextField("Enter Your Email", text: $viewModel1.email)
+                            .padding()
+                            .background(
+                                VStack {
+                                    Color.white.opacity(0.4)
+                                    Rectangle().frame(height: 1).foregroundColor(.gray)
+                                }
+                            )
+                        
+                        TextField("Enter Your Name", text: $viewModel1.name)
+                            .padding()
+                            .background(
+                                VStack {
+                                    Color.white.opacity(0.4)
+                                    Rectangle().frame(height: 1).foregroundColor(.gray)
+                                }
+                            )
+                        
+                        SecureField("Enter Your Password", text: $viewModel1.password)
+                            .padding()
+                            .background(
+                                VStack {
+                                    Color.white.opacity(0.4)
+                                    Rectangle().frame(height: 1).foregroundColor(.gray)
                                     
-                            }
-                        )
-                    Button(action: {
-                        viewModel1.signIn()
-                    }) {
-                        Text("Sign in")
-                            .font(.headline)
-                            .foregroundColor(.white)
-                            .frame(maxWidth: .infinity)
-                            .frame(height: 55)
-                            .background(Color.black)
-                            .cornerRadius(10)
+                                }
+                            )
+                        
+        Button(action: {
+        viewModel1.signIn() { success in
+            if success {
+                showOnboarding3 = true
                     }
+            }
+           }) {
+           Text("Sign in")
+                .font(.headline)
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity)
+                .frame(height: 55)
+                .background(Color.black)
+                .cornerRadius(10)
                 }
-                .padding(.top, 350)
-                .frame(width: UIScreen.main.bounds.width * 0.8)
-               
-                
-                
-                
-                //bottom stuff
+                                                
+                // Updated NavigationLink initializer
+    NavigationLink("", destination: Onboarding3(), isActive: $showOnboarding3)
+            .hidden()
+        }
+          .offset(y: 120)
+          .padding(.bottom, 50)
+          .frame(width: UIScreen.main.bounds.width * 0.8)
+                    
+                    
+                    
+                    //bottom stuff
                     
                     HStack (spacing: 130){
                         ZStack(alignment: .leading){
-                                           
+                            
                             Rectangle()
-                        .frame(width: 150, height: 15)
+                                .frame(width: 150, height: 15)
                                 .foregroundColor(.gray)
                                 .cornerRadius(7)
                             
                             
                             Rectangle()
-                            .frame(width:50, height: 15)
-                            .foregroundColor(.black)
-                            .cornerRadius(7)
-                                               
-                                       }
+                                .frame(width:50, height: 15)
+                                .foregroundColor(.black)
+                                .cornerRadius(7)
+                            
+                        }
                         
                         
                         
@@ -123,15 +136,16 @@ struct Onboarding2: View {
                         
                     }
                     .position(x: geometry.size.width / 2, y: geometry.size.height)
-                
-               
-                
+                    
+                    
+                    
+                }
+                .edgesIgnoringSafeArea(.all)
+                .background(Color.white)
             }
-            .edgesIgnoringSafeArea(.all)
-            .background(Color.white)
         }
     }
-    }
+}
     struct Onboarding2_Previews: PreviewProvider {
         static var previews: some View {
             Onboarding2()
@@ -143,22 +157,26 @@ final class Login1ViewModel : ObservableObject{
     @Published var email = ""
     @Published var password = ""
     @Published var name = ""
-    func signIn() {
-        guard !email.isEmpty, !password.isEmpty, !name.isEmpty else{
-            print("No email or password found.")
+    func signIn(completion: @escaping (Bool) -> Void) {
+        guard !email.isEmpty, !password.isEmpty, !name.isEmpty else {
+            print("Please fill out all required fields.")
+            completion(false)
             return
         }
-        Task{
-            do{
+        
+        Task {
+            do {
                 let returnedUserData: () = try await AuthenticationManager.shared.createUser(email: email, password: password, name: name)
                 print("Success")
                 print(returnedUserData)
-            }catch{
+                completion(true)
+            } catch {
                 print("Error: \(error)")
-                
+                completion(false)
             }
         }
     }
+
 }
 
 
---
+++ Erra/Views/Onboarding3.swift
@@ -0,0 +1,20 @@
+//
+//  Onboarding3.swift
+//  Erra
+//
+//  Created by Heidi Schultz on 9/1/23.
+//
+
+import SwiftUI
+
+struct Onboarding3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Onboarding3_Previews: PreviewProvider {
+    static var previews: some View {
+        Onboarding3()
+    }
+}
