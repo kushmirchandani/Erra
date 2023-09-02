ec42bd3b8c7c5de5d5c0c6ca4a87e51ec4f70edb
Kush Mirchandani <kmirchandani2006@gmail.com>
2023-09-02 10:02:52 -0400

    new views

---
+++ Erra/Views/AddHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  AddHomeView.swift
+//  Erra
+//
+//  Created by Kush Mirchandani on 9/2/23.
+//
+
+import SwiftUI
+
+struct AddHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddHomeView()
+}
