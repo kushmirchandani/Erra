7dc307124f29562ba9d4d126c8506738555c8f1b
Kush Mirchandani <kmirchandani2006@gmail.com>
2023-09-02 10:11:28 -0400

    Another Update

---
+++ Erra/Views/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  Erra
+//
+//  Created by Kush Mirchandani on 9/2/23.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
