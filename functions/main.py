c13731ad270510a8c0ab111fb1b8f500411c96f2
Heidi Schultz <heidischultz23@gmail.com>
2023-09-30 14:47:41 -0400

    ok

--- functions/main.py
+++ functions/main.py
@@ -2,10 +2,14 @@
 # To get started, simply uncomment the below code or create your own.
  Deploy with `firebase deploy`
 
-from firebase_functions import https_fn
-from firebase_admin import initialize_app
+# The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
+from firebase_functions import firestore_fn, https_fn
 
-initialize_app()
+# The Firebase Admin SDK to access Cloud Firestore.
+from firebase_admin import initialize_app, firestore
+import google.cloud.firestore
+
+app = initialize_app()
 
  @https_fn.on_request()
 def on_request_example(req: https_fn.Request) -> https_fn.Response:
