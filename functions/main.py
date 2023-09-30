cdfc0717dbc4e0cd35f570cc25b5b1979cc27270
Heidi Schultz <heidischultz23@gmail.com>
2023-09-30 14:56:42 -0400

    hjhik

--- functions/main.py
+++ functions/main.py
@@ -9,4 +9,4 @@ initialize_app()
 
  @https_fn.on_request()
 def on_request_example(req: https_fn.Request) -> https_fn.Response:
-    return https_fn.Response("Hello world!")
+   return https_fn.Response("Hello world!")
